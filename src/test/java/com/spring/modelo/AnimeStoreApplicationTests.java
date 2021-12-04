cf60fe9af27dcf9d8cd1fb63cff86f344600ca7e
roger <rocas@gmail.com>
2021-12-03 20:19:05 -0500

    Commit proyecto Java Crud

---
+++ src/test/java/com/spring/modelo/AnimeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.modelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
